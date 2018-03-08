e467d70a46034573e14b0f25fb14b2d4b010f7b2
Keegan18 <kolbeinsen.tafaoa@gmail.com>
2018-03-07 14:10:56 -1000

    Update Final

--- app.js/app.js
+++ app.js/app.js
@@ -1,2 +1,14 @@
-console.log(Test);
+var item1 = "Pencil"
+var item2 = "Eraser"
+var item3 = "Calculator"
+var item4 = "Watch"
 
+var ray = [item1, item2, item3, item4];
+
+var boss = 56;
+
+ray.pop(.push(boss));
+
+document.getElementById("fourth") = 
+
+console.log(Test);
